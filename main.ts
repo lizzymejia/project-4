4ab0c0d6f9ebca98d7a2076d4c488c1027c9cbb7
lizzymejia <72759970+lizzymejia@users.noreply.github.com>
2021-02-02 16:46:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+while (true) {
+    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0) {
+        light.setAll(light.rgb(78, 129, 255))
+        music.playMelody
+    } else {
+        light.clear()
+    }
+    
+}
