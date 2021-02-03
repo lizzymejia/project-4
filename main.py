571056580753f24c87ca60fd8270aecc551ac0d8
lizzymejia <72759970+lizzymejia@users.noreply.github.com>
2021-02-03 14:29:48 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 while True:
-    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z) < 0:
+    if input.acceleration(Dimension.Z) > 0:
         light.set_all(light.rgb(78,129,255))
-        music.play_melody
+        
     else:
         light.clear()
+        
--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 while (true) {
-    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0) {
+    if (input.acceleration(Dimension.Z) > 0) {
         light.setAll(light.rgb(78, 129, 255))
-        music.playMelody
     } else {
         light.clear()
     }
